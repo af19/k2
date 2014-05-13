7be3d923924cdce0cfabc0f5766a8cb69243910d
amir farzan <amir.ff@gmail.com>
2014-05-13 01:18:16 -0700

    removed scaffolds.css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$( document ).ready(function() {
+	$("#email-exists").hide();
+  if ($(".field_with_errors").length > 0) {
+		$(".btn").hide();
+	}
+	if ($("#error_explanation").length > 0) {
+		$("#error_explanation, .site-wrapper").hide();
+		$("#email-exists").show();
+	}
+
+});
