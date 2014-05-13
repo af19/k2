75176660b7889db5d79fbc82c54877936a432c0f
amir farzan <amir.ff@gmail.com>
2014-05-13 00:45:47 -0700

    added bootstrap

--- app/assets/javascripts/jq.js
+++ app/assets/javascripts/jq.js
@@ -1,6 +1,11 @@
 $( document ).ready(function() {
+	$("#email-exists").hide();
   if ($(".field_with_errors").length > 0) {
-	$(".actions").hide();
-}
+		$(".btn").hide();
+	}
+	if ($("#error_explanation").length > 0) {
+		$("#error_explanation, .site-wrapper").hide();
+		$("#email-exists").show();
+	}
 
 });
--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,3 +1,6 @@
 class Sub < ActiveRecord::Base
 	validates :email, uniqueness: { message: "You're already subscribed. Thanks!"}
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+    message: "That email doesn't look right. Please try again." }
+            
 end
